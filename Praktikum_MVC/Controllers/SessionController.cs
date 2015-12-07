96899de5d76c5f2ee05093603657e5894c6858b1
Faerbit <faerbit@gmail.com>
2015-12-07 03:56:53 +0100

    Added session management.

---
+++ Praktikum_MVC/Controllers/SessionController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Praktikum_MVC.Controllers
+{
+    public class SessionController : Controller
+    {
+        // GET: Session
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Home", new { error = "login" });
+        }
+        // POST: Session
+        [HttpPost]
+        public ActionResult Index(Praktikum_MVC.Models.User user)
+        {
+            if (Session["authenticated"] != null && Session["authenticated"] != "") {
+                Session["authenticated"] = "";
+                return RedirectToAction("Index", "Home", new { logout = "true" });
+            }
+            if (user.checkPassword()) {
+                Session["authenticated"] = user.username;
+                return RedirectToAction("Index", "Home", new { login = "true" });
+            }
+            else {
+                ViewBag.error = true;
+            }
+            return View();
+        }
+    }
+}
---
+++ Praktikum_MVC/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Praktikum_MVC.Models
+{
+    public class User
+    {
+        public string username {get; set;}
+        public string password {get; set; }
+
+        public bool checkPassword() {
+            if (username == "" || password == "") {
+                return false;
+            }
+            string connStr = ConfigurationManager.ConnectionStrings["dbConnString"].ConnectionString;
+            var connection = new SqlConnection(connStr);
+            var query = @"
+                    select convert(varchar(32), Passwort, 2) as Passwort from Benutzer where Nickname = @username";
+            var sqlcmd = new SqlCommand(query, connection);
+            sqlcmd.Parameters.AddWithValue("@username", username);
+            connection.Open();
+            SqlDataReader reader = sqlcmd.ExecuteReader();
+            if (reader.Read()) {
+                if(System.Web.Helpers.Crypto.Hash(password, "md5").ToString() == reader["Passwort"].ToString()) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
