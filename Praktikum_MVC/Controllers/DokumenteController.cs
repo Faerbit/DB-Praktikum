3a0bc3d0865d5e94645bb56cf705508390d4245f
Faerbit <faerbit@gmail.com>
2016-01-16 19:43:49 +0100

    Stuffy stuff.

---
+++ Praktikum_MVC/Controllers/DokumenteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Praktikum_MVC.Controllers
+{
+    public class DokumenteController : Controller
+    {
+        // GET: Dokumente
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
