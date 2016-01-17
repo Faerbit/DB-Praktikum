c58dc8b8bb960f8b99ac803c31f6002e7a1fd987
Faerbit <faerbit@gmail.com>
2016-01-17 01:45:43 +0100

    asdf

--- Praktikum_MVC/Controllers/DokumenteController.cs
+++ Praktikum_MVC/Controllers/DokumenteController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace Praktikum_MVC.Controllers
@@ -12,6 +13,18 @@ namespace Praktikum_MVC.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        // GET: Dokumente/Statistik
+        public ActionResult Statistik()
+        {
+            var chart = new Chart(width: 600, height: 400)
+               .AddSeries(
+                   chartType: "pie",
+                   legend: "Rainfall",
+                   xValue: new[] { "Jan", "Feb" },
+                   yValues: new[] { "5", "10" }
+               ).GetBytes("png");
+            return File(chart, "image/png");
         }
     }
 }
---
+++ Praktikum_MVC/Models/Dokumente.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Praktikum_MVC.Models
+{
+    public class Dokumente
+    {
+    }
+}
