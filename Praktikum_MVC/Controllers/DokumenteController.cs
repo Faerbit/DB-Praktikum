d463daa2190c85e4f14a489361df85c636abdcc3
Faerbit <faerbit@gmail.com>
2016-01-17 14:12:51 +0100

    ölasdk

--- Praktikum_MVC/Controllers/DokumenteController.cs
+++ Praktikum_MVC/Controllers/DokumenteController.cs
@@ -5,24 +5,50 @@ using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
 
+using Praktikum_MVC.Models;
+
 namespace Praktikum_MVC.Controllers
 {
     public class DokumenteController : Controller
     {
         // GET: Dokumente
+        [HttpGet]
         public ActionResult Index()
         {
+            ViewBag.Kategorien = Dokumente.getAllKategorien();
             return View();
         }
+
+        // POST: Dokumente
+        [HttpPost]
+        public ActionResult Index(Kategorie kategorie)
+        {
+            ViewBag.Kategorien = Dokumente.getAllKategorien();
+            if (Request.Form["kategorie"] != null)
+            {
+                kategorie = new Kategorie { kategorie = Request.Form["kategorie"] };
+                ViewBag.Dokumente = Dokumente.getDokumenteByKategorie(kategorie);
+            }
+            return View();
+        }
+
         // GET: Dokumente/Statistik
+        [HttpGet]
         public ActionResult Statistik()
         {
-            var chart = new Chart(width: 600, height: 400)
+            var xValues = new List<string>();
+            var yValues = new List<string>();
+            var statistik = Dokumente.getKategorieStatistik();
+            for(int i = 0; i < statistik.Count; i++) {
+                xValues.Add(statistik[i].Item1);
+                yValues.Add(statistik[i].Item2);
+            }
+            var chart = new Chart(width: 500, height: 500)
                .AddSeries(
                    chartType: "pie",
-                   legend: "Rainfall",
-                   xValue: new[] { "Jan", "Feb" },
-                   yValues: new[] { "5", "10" }
+                   legend: "Dokumente",
+                   xValue: xValues,
+                   yValues: yValues
                ).GetBytes("png");
             return File(chart, "image/png");
         }
--- Praktikum_MVC/Models/Dokumente.cs
+++ Praktikum_MVC/Models/Dokumente.cs
@@ -1,11 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace Praktikum_MVC.Models
 {
+    public class Kategorie
+    {
+        public string kategorie { get; set; }
+    }
+
     public class Dokumente
     {
+        public string titel { get; set; }
+        public string datum { get; set; }
+        public string groesse { get; set; }
+        public string bereitsteller { get; set; }
+
+        public static List<Dokumente> getDokumenteByKategorie(Kategorie kategorie)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["dbConnString"].ConnectionString;
+            var connection = new SqlConnection(connStr);
+            var query = @"
+                select Dokumente.Titel, Dokumente.Bereitstellungsdatum, DATALENGTH(Datei) as Groesse, Benutzer.Vorname, Benutzer.Nachname
+                from Dokumente join Benutzer on Dokumente.Benutzer = Benutzer.Nickname
+                where Kategorie = @kategorie";
+            var sqlcmd = new SqlCommand(query, connection);
+            sqlcmd.Parameters.AddWithValue("@kategorie", kategorie.kategorie);
+            connection.Open();
+            SqlDataReader reader = sqlcmd.ExecuteReader();
+            List<Dokumente> result = new List<Dokumente>();
+            while (reader.Read())
+            {
+                string groesse = "";
+                if (reader["Groesse"].ToString() == "") {
+                    groesse = "0 Bytes";
+                }
+                else {
+                    groesse = reader["Groesse"].ToString() + " Bytes";
+                }
+                Dokumente dokument = new Dokumente
+                {
+                    titel = reader["Titel"].ToString(),
+                    bereitsteller = reader["Vorname"].ToString() + " " + reader["Nachname"].ToString(),
+                    datum = reader["Bereitstellungsdatum"].ToString(),
+                    groesse = groesse
+                };
+                result.Add(dokument);
+            }
+            connection.Close();
+            return result;
+        }
+
+        public static List<string> getAllKategorien() {
+            string connStr = ConfigurationManager.ConnectionStrings["dbConnString"].ConnectionString;
+            var connection = new SqlConnection(connStr);
+            var query = @"
+                select Kategorie
+                from Dokumente 
+                group by Kategorie
+                order by Kategorie";
+            var sqlcmd = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = sqlcmd.ExecuteReader();
+            List<string> result = new List<string>();
+            while (reader.Read())
+            {
+                result.Add(reader["Kategorie"].ToString());
+            }
+            connection.Close();
+            return result;
+        }
+
+        public static List<Tuple<string, string>> getKategorieStatistik()
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["dbConnString"].ConnectionString;
+            var connection = new SqlConnection(connStr);
+            var query = @"
+                select Kategorie, count(Kategorie) as count
+                from Dokumente 
+                group by Kategorie
+                order by Kategorie";
+            var sqlcmd = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = sqlcmd.ExecuteReader();
+            List<Tuple<string, string>> result = new List<Tuple<string, string>>();
+            while (reader.Read())
+            {
+                var tuple = new Tuple<string, string>(reader["Kategorie"].ToString() 
+                    + " (" + reader["count"].ToString() + ")",
+                    reader["count"].ToString());
+                result.Add(tuple);
+            }
+            connection.Close();
+            return result;
+        }
     }
 }
