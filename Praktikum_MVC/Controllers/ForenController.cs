1a283d4ae48d0e28aa477846570ae79efefce120
Faerbit <faerbit@gmail.com>
2016-01-18 01:33:45 +0100

    hasngnasn

--- Praktikum_MVC/Controllers/ForenController.cs
+++ Praktikum_MVC/Controllers/ForenController.cs
@@ -15,6 +15,7 @@ namespace Praktikum_MVC.Controllers
         private praktikumEntities db = new praktikumEntities();
 
         // GET: Foren
+        [HttpGet]
         public ActionResult Index(int id = 0)
         {
             var foren = db.Foren.Where(f => f.OberforumID == null);
@@ -35,20 +36,51 @@ namespace Praktikum_MVC.Controllers
         }
 
         // GET: Foren/Diskussion/5
-        public ActionResult Diskussion(int? id)
+        [HttpGet]
+        public ActionResult Diskussion(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
             var diskussion = db.Diskussionen.Find(id);
             if (diskussion == null)
             {
                 return HttpNotFound();
             }
+            db.Gesichtet(id);
             ViewBag.Beitraege = db.Beiträge.Where(b => b.DiskussionsID == id);
             ViewBag.Diskussion = diskussion;
             return View();
+        }
+
+        // POST: Foren/NewPost/5
+        [HttpPost]
+        public ActionResult NewPost(int id, Beiträge beitrag)
+        {
+            beitrag.Benutzer = (string) Session["authenticated"];
+            beitrag.Änderungsdatum = DateTime.Now;
+            beitrag.DiskussionsID = id;
+            db.Beiträge.Add(beitrag);
+            db.SaveChanges();
+            return RedirectToAction("Diskussion", "Foren", new { id = id });
+        }
+
+        // POST: Foren/NewDiscussion/5
+        [HttpGet]
+        public ActionResult NewDiscussion(int id)
+        {
+            var diskussion = db.Diskussionen.Find(id);
+            if (diskussion == null)
+            {
+                return HttpNotFound();
+            }
+            return View();
+        }
+
+        // POST: Foren/NewDiscussion/5
+        [HttpPost]
+        public ActionResult NewDiscussion(int id, Beiträge beitrag)
+        {
+            beitrag.Benutzer = (string)Session["authenticated"];
+            //db.SaveChanges();
+            return RedirectToAction("Diskussion", "Foren", new { id = id });
         }
 
         /*
